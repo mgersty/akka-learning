a04ba0f0ea85b028aba27f55afc0e6144033811b
Matthew P Gerstemeier mpg26 <mgerstemeier@cas.org>
2019-03-20 16:15:18 -0400

    reverting 4a6943e

--- src/main/scala/org/gersty/akkalearning/HelloAkkaActorSystem.scala
+++ src/main/scala/org/gersty/akkalearning/HelloAkkaActorSystem.scala
@@ -5,7 +5,7 @@ import akka.actor.ActorSystem
 object HelloAkkaActorSystem {
   
   def main(args: Array[String]): Unit ={
-    val actorSystem = ActorSystem("HelloAkka1")
+    val actorSystem = ActorSystem("HelloAkka")
     println(actorSystem)    
   }
   
